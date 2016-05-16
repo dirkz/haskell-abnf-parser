446c007268949363867412ff01a03e76cd9c76ec
Dirk Zimmermann <dirk@pep-project.org>
2016-05-16 20:43:54 +0200

    bin-val, dec-val, hex-val

--- src/ParseABNF.hs
+++ src/ParseABNF.hs
@@ -4,10 +4,17 @@ import Text.ParserCombinators.Parsec
 import Data.Char (digitToInt, ord)
 
 data RepeatCount = Count Int | Infinity
+  deriving (Show, Eq, Ord)
 
-data Token = Skip -- ^ Just skip/ignore that token
-           | Repeat RepeatCount RepeatCount
-           | Range Int Int
+data Value = ValueSingle Int
+           | ValueSeq [Int]
+           | ValueRange Int Int
+  deriving (Show, Eq, Ord)
+
+data Token = TokenSkip -- ^ Just skip/ignore that token.
+           | TokenRepeat RepeatCount RepeatCount
+           | TokenRange Int Int
+  deriving (Show, Eq, Ord)
 
 parseRuleName :: Parser String
 parseRuleName = do
@@ -17,11 +24,11 @@ parseRuleName = do
 
 -- | comment
 skipComment :: Parser Token
-skipComment = char ';' >> many (parseWSP <|> parseVCHAR) >> newline >> return Skip
+skipComment = char ';' >> many (parseWSP <|> parseVCHAR) >> newline >> return TokenSkip
 
 -- | c-nl
 skipCommentOrNewline :: Parser Token
-skipCommentOrNewline = skipComment <|> (newline >> return Skip)
+skipCommentOrNewline = skipComment <|> (newline >> return TokenSkip)
 
 -- | repeat
 parseRepeat :: Parser Token
@@ -29,14 +36,19 @@ parseRepeat = do
   c1 <- maybe (Count 0) Count <$> parseMaybeInt
   char '*'
   c2 <- maybe Infinity Count <$> parseMaybeInt
-  return $ Repeat c1 c2
+  return $ TokenRepeat c1 c2
 
--- | hex-val
-parseHexVal :: Parser Token
-parseHexVal = do
-  char 'x'
-  let h1 = many1 parseHEXDIG
-  return $ Range 0 0
+-- | Base: bin-val
+parseBinVal :: Parser Value
+parseBinVal = parseGenericVal parseHEXDIG 2 'b'
+
+-- | Base: dec-val
+parseDecVal :: Parser Value
+parseDecVal = parseGenericVal parseHEXDIG 10 'd'
+
+-- | Base: hex-val
+parseHexVal :: Parser Value
+parseHexVal = parseGenericVal parseHEXDIG 16 'x'
 
 -- | Base: DIGIT
 parseDigit :: Parser Int
@@ -57,6 +69,26 @@ parseHEXDIG = parseDigit <|> hexDigit
     minusA = flip (-) $ ord 'A' - 10
     hexDigit = minusA <$> ord <$> oneOf ['A'..'F']
 
+-- | Helper: hex-val, dec-val, bin-val
+parseGenericVal :: Parser Int -> Int -> Char -> Parser Value
+parseGenericVal parser base charPrefix = do
+  char charPrefix
+  h1 <- toInt base <$> many1 parser
+  o <- option Nothing $ Just <$> parseDots <|> Just <$> parseDash
+  return $
+    case o of
+      Nothing -> ValueSingle h1
+      Just vs@(v:_) ->
+        case v of
+          (ValueRange h2 _) -> ValueRange h1 h2
+          otherwise -> ValueSeq $ h1 : extract vs
+  where
+    extract :: [Value] -> [Int]
+    extract = map extractInt
+    extractInt (ValueSingle v) = v
+    parseDots = many1 $ parseDotDigits parser base
+    parseDash = count 1 (parseDashDigits parser base)
+
 -- | Helper: Parse a list of Ints into an Int, with the given base.
 toInt :: Int -> [Int] -> Int
 toInt base = fst . foldr fn (0, 1)
@@ -70,6 +102,23 @@ parseMaybeInt = do
   if null ds then return Nothing
              else return $ Just $ toInt 10 ds
 
+-- | Helper: Can parse something like "-" 1*HEXDIG or "." 1*HEXDIG (or DIGIT, BIT).
+-- | The HEXDIG depends on the given parser, the result also on the base.
+parseDotDashDigits :: Parser Int -> Int -> Char -> Parser Value
+parseDotDashDigits parser base c = do
+  char c
+  h1 <- toInt base <$> many1 parser
+  return $ if c == '.' then ValueSingle h1 else ValueRange h1 h1
+
+-- | Helper: Parse "-" 1*HEXDIG (or DIGIT, BIT)
+parseDashDigits :: Parser Int -> Int -> Parser Value
+parseDashDigits parser base = parseDotDashDigits parser base '-'
+
+-- | Helper: Parse "." 1*HEXDIG (or DIGIT, BIT)
+parseDotDigits :: Parser Int -> Int -> Parser Value
+parseDotDigits parser base = parseDotDashDigits parser base '.'
+
+-- | Helper: Debug parse a function
 parseDebug :: Parser a -> String -> Either ParseError a
 parseDebug parser = parse parser "debug"
 
